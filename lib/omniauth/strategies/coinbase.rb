e4ec2923e1c51b8906e7ab6d60ac8595adb63ece
Carlos Cardona <cgcardona@gmail.com>
2014-02-17 17:52:46 -0800

    Revert changes.

--- lib/omniauth/strategies/coinbase.rb
+++ lib/omniauth/strategies/coinbase.rb
@@ -8,6 +8,28 @@ module OmniAuth
               :site => 'https://coinbase.com',
               :proxy => ENV['http_proxy'] ? URI(ENV['http_proxy']) : nil
       }
+
+      uid { raw_info['id'] }
+
+      info do
+        {
+          :id => raw_info['id'],
+          :name => raw_info['name'],
+          :email => raw_info['email'],
+          :balance => raw_info['balance']['amount']
+        }
+      end
+
+      extra do
+        { :raw_info => raw_info }
+      end
+
+      def raw_info
+        @raw_info ||= MultiJson.load(access_token.get('/api/v1/users').body)['users'][0]['user']
+      rescue ::Errno::ETIMEDOUT
+        raise ::Timeout::Error
+      end
+
     end
   end
 end
